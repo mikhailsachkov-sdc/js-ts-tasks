641d41fad6026d1d7f9acd4a3db099d728ff6804
Mikhail Sachkov <sachkov.mikhail@student.ehu.lt>
2024-11-07 16:59:36 +0000

    feat: use reduce directly on string

--- src/digitsSum.js
+++ src/digitsSum.js
@@ -4,8 +4,5 @@
  * @returns {number}
  */
 module.exports.digitsSum = function digitsSum(n) {
-  return n
-    .toString()
-    .split('')
-    .reduce((partialSum, a) => partialSum + (+a || 0), 0);
+  return Array.prototype.reduce.call(n.toString(), (partialSum, a) => partialSum + (+a || 0), 0);
 };
