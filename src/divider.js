7e31e8de343d59f93e9db8bf4680e8a7b52d6710
Mikhail Sachkov <sachkov.mikhail@student.ehu.lt>
2024-11-07 17:00:09 +0000

    fix: return type in jsdoc

--- src/divider.js
+++ src/divider.js
@@ -3,7 +3,7 @@
  * Return a string formatted "wholePart remainder"
  * @param {number} n - number
  * @param {number} k - number
- * @returns {number}
+ * @returns {string}
  */
 module.exports.divider = function divider(n, k) {
   const wholePart = Math.floor(n / k);
